d49efbe52aec3607251464762ed78d98c0b069de
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-06-19 11:07:59 +0300

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const lodash = require("lodash");
+require("./style.css"); 
+const Component = require("./Component");
+const resultOfSummaTwoNumbers = 5 + 2;
+console.log(lodash.random());
+console.log(resultOfSummaTwoNumbers);
+const comp = new Component();
+comp.render();
